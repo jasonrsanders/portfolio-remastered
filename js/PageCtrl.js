c56a055bec71e93f7b46540aa6e6c9b7fb77c130
Talon Hughes <talon.hughes@gmail.com>
2017-01-12 16:26:44 -0700

    fixed the non working image slider

--- js/PageCtrl.js
+++ js/PageCtrl.js
@@ -2,27 +2,5 @@ app.controller('PageCtrl', function($scope) {
 
     console.log("Page Controller reporting for duty.");
     $scope.pageClass = 'page-effect';
-
-    // MAC MOCK-UP SCROLLER #1
-    // $(".handle").draggable({
-    //     axis: "x",
-    //     containment: "parent",
-    //     drag: function() {
-    //         var position = $(this).position();
-    //         var positionExtra = position.left + 6;
-    //         $(".coverImage").width(positionExtra + "px");
-    //     }
-    // });
-
-    // MAC MOCK-UP SCROLLER #2
-    $(".handle2").draggable({
-        axis: "x",
-        containment: "parent",
-        drag: function() {
-            var position = $(this).position();
-            var positionExtra = position.left + 6;
-            $(".coverImage2").width(positionExtra + "px");
-        }
-    });
     
 });
